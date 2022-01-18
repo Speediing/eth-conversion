ce3726158b8dab4af6eafc9e78a5323ae2a45bb7
Jason Wiker <jason.wiker@gmail.com>
2022-01-18 15:51:05 -0700

    init commit + header

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24">
+      <div className="lg:text-center">
+        <h2 className="text-base text-green-300 font-semibold tracking-wide uppercase">
+          Eth Conversion Tool
+        </h2>
+        <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+          A better way to send money
+        </p>
+        <p className="mt-4 max-w-2xl text-xl text-gray-300 lg:mx-auto">
+          Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+          voluptatum cupiditate veritatis in accusamus quisquam.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Header;
