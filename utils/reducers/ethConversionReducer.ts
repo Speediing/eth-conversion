32b4de7a3592667b087f1d5befafbd96bc3ef6d9
Jason Wiker <jason.wiker@gmail.com>
2022-01-18 19:03:38 -0700

    fix build

--- utils/reducers/ethConversionReducer.ts
+++ utils/reducers/ethConversionReducer.ts
@@ -2,5 +2,7 @@ import BigNumber from "bignumber.js";
 
 export const ethConversionReducer = (_: any, action: any) => {
   if (!action.value || isNaN(action.value)) return null;
-  return new BigNumber(action.value * 1.0).dividedBy(action.multiplier);
+  return new BigNumber(action.value * 1.0)
+    .dividedBy(action.multiplier)
+    .toNumber();
 };
